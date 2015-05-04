0152ebee4ab7ceb18bcc765d766677f865036542
Tyler Goodman <finlist@gmail.com>
2015-05-04 13:23:35 -0700

    node done

--- src/main/java/com/fp/Runner.java
+++ src/main/java/com/fp/Runner.java
@@ -1,9 +1,14 @@
 package com.fp;
 
+import java.util.Arrays;
+
 public class Runner
 {
-    public static void main(String args[])
-    {
-        System.out.println("Hello World!");
-    }
+    public static void main(String[] args) {
+  		Node n = new Node();
+  		System.out.println(n.string);
+  		n.shuffle();
+  		System.out.println(n.string);
+
+  	}
 }
